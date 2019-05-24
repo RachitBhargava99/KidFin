3916d3fcdee9da826777977c25ccb8e627554697
RachitBhargava99 <rachitbhargava99@gmail.com>
2019-05-23 21:44:37 -0400

    add_kid_account() staged for review

--- backend/transactions/utils.py
+++ backend/transactions/utils.py
@@ -62,28 +62,3 @@ def process_purchase(accountID, merchantID, amount, date=datetime.now().strftime
     response = requests.post(url, json=payload)
 
     return response.json()['code'] == 201
-
-
-def addAccount(customerID, accountName, balance, accountNumber):
-    #   Purpose: Creates an account with a given customerID
-    #
-    #   Inputs:
-    #   customerID (string)
-    #   account_name (string)
-    #   balance (integer)
-    #   account_number (string) - a 16 digit string that is essentially the 'credit card number'; make it anything
-    #   assumptions- type: checking, rewards: 0
-
-    url = "http://api.reimaginebanking.com/customers/" + customerID + "/accounts?key=bb72fd1c5dee869a93bd5c6ba281cadb"
-
-    payload = {
-        "type": "Checking",
-        "nickname": accountName,
-        "rewards": 0,
-        "balance": balance,
-        "account_number": accountNumber
-    }
-
-    response = requests.post(url, json=payload)
-
-    return response.text
